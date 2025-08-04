cd4ae0160cded98749673ced7911e008d40941c6
duynguyeneiu <155942227+duynguyeneiu@users.noreply.github.com>
2025-08-04 19:00:34 +0700

    Homepage, upgrate project structure, change link

---
+++ hotelbooking/src/main/java/com/hotel/hotelbooking/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.hotel.hotelbooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homepage() {
+        return "home/homepage";
+    }
+}
