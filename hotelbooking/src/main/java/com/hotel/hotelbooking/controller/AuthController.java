834e09d23bf9c9da3ea2ce6f626ba6f3fc17356e
NguyenVy2131 <vy.nguyenthi.cit21@eiu.edu.vn>
2025-08-25 10:55:04 +0700

    login button
    after login, in header admin have admin, circle profile, client have circle profile instead of login and register

--- hotelbooking/src/main/java/com/hotel/hotelbooking/controller/AuthController.java
+++ hotelbooking/src/main/java/com/hotel/hotelbooking/controller/AuthController.java
@@ -77,6 +77,6 @@ public class AuthController {
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        return "redirect:/auth/login?logout";
+        return "redirect:/";
     }
 }
